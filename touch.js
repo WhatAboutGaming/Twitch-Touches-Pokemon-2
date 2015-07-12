9d2ce79631daab715f6e4a9da89b007883e17275
Waggle <tsijesuswlg@gmail.com>
2015-07-12 04:01:22 +0000

    Alpha Sapphire Fix v3

--- touch.js
+++ touch.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           Twitch touches pokemon
-// @namespace      https://github.com/lostcoaster/twitch-touches-pokemon
+// @namespace      https://raw.githubusercontent.com/WhatAboutGaming/Twitch-Touches-Pokemon-2/master/touch.user.js
 // @author         lostcoaster
 // @author         1.8a
 // @description    A tool adding a touch overlay onto the stream of twitchplayspokemon.
@@ -14,5 +14,5 @@
 
 
 //This file is obsoleted and kept only for compatibility. The working file was renamed to touch.user.js. Thanks to MattiasBuelens's suggestion.
-//If you are using this, turn to https://raw.githubusercontent.com/lostcoaster/twitch-touches-pokemon/master/touch.user.js instead
+//If you are using this, turn to https://raw.githubusercontent.com/WhatAboutGaming/Twitch-Touches-Pokemon-2/master/touch.user.js instead
 (function(){document.body.appendChild(document.createElement('script')).src='https://raw.githubusercontent.com/WhatAboutGaming/Twitch-Touches-Pokemon-2/master/touch.user.js';})();
